15afb768767b6118e6e3fa075e03b448b3fb59e0
Livio Ribeiro <livioribeiro@outlook.com>
2023-12-21 20:54:26 -0300

    atualiza mensagem

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const HOST = '0.0.0.0';
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Teste de mensagem 3');
+  res.send('Teste de mensagem 4');
 });
 
 app.listen(PORT, HOST, () => {
